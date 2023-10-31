2708b4e32a4080897f6fdf242e5fc7e04fd251a2
Hambak-note <dudghks5722@gmail.com>
2023-10-31 23:39:38 +0900

    [feat] 스프링 시큐리티 기본

---
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.example.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize 어노테이션 활성화
+public class SecurityConfig  {
+
+    //@Bean 선언해주면 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
+    @Bean
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
+
+//   @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//       http.csrf().disable();
+//       http.authorizeRequests()
+//               .antMatchers("/user/**").authenticated()
+//               .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+//               .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+//               .anyRequest().permitAll();
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//
+//   }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/user/**").authenticated()
+                                .requestMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                                .requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                                .anyRequest().permitAll());
+        http
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/loginForm")
+                                .usernameParameter("username")
+                                .loginProcessingUrl("/login") //login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해준다.
+                                .defaultSuccessUrl("/"));
+
+        return http.build();
+    }
+
+
+}
